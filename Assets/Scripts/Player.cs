36606f05062e5ea45d47cfd12a58ed15613a6cac
Michael Petty <michaelp1985@live.com>
2020-01-15 23:20:15 -0600

    Added player movement and a new package.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 5f;
+
+    Rigidbody2D playerBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        FlipSprite();
+    }
+
+    private void Run()
+    {        
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, playerBody.velocity.y);
+        playerBody.velocity = playerVelocity;
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(playerBody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(playerBody.velocity.x), 1f);
+        }
+    }
+}
